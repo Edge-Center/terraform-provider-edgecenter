958c7730a5c633fa5382d12657e2785dbc82db8f
Aleksandr Strelkov <alexander.strelkov@hotmail.com>
2023-11-10 15:15:50 +0500

    Fixed imports

--- edgecenter/resource_edgecenter_cdn_rule.go
+++ edgecenter/resource_edgecenter_cdn_rule.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
+    cdn "github.com/Edge-Center/edgecentercdn-go/edgecenter"
 	"github.com/Edge-Center/edgecentercdn-go/rules"
 )
 
