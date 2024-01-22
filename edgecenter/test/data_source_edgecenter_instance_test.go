fed36e59af20843bf46a282224d8b799f9f5edc8
a.semenov <a.semenov@comagic.dev>
2024-01-22 12:20:59 +0300

    (CLOUDDEV-472): Refactor test for Instance to new go client

--- edgecenter/test/data_source_edgecenter_instance_test.go
+++ edgecenter/test/data_source_edgecenter_instance_test.go
@@ -3,45 +3,33 @@
 package edgecenter_test
 
 import (
+	"context"
 	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 
-	edgecloud "github.com/Edge-Center/edgecentercloud-go"
-	"github.com/Edge-Center/edgecentercloud-go/edgecenter/image/v1/images"
-	"github.com/Edge-Center/edgecentercloud-go/edgecenter/instance/v1/instances"
-	"github.com/Edge-Center/edgecentercloud-go/edgecenter/instance/v1/types"
-	"github.com/Edge-Center/edgecentercloud-go/edgecenter/task/v1/tasks"
-	"github.com/Edge-Center/edgecentercloud-go/edgecenter/volume/v1/volumes"
-	"github.com/Edge-Center/terraform-provider-edgecenter/edgecenter"
+	edgecloudV2 "github.com/Edge-Center/edgecentercloud-go/v2"
+
+	utilV2 "github.com/Edge-Center/edgecentercloud-go/v2/util"
 )
 
 func TestAccInstanceDataSource(t *testing.T) {
 	t.Parallel()
+	ctx := context.Background()
 	cfg, err := createTestConfig()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	clientVolume, err := createTestClient(cfg.Provider, edgecenter.VolumesPoint, edgecenter.VersionPointV1)
+	imgs, _, err := cfg.CloudClient.Images.List(ctx, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	clientImage, err := createTestClient(cfg.Provider, edgecenter.ImagesPoint, edgecenter.VersionPointV1)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	imgs, err := images.ListAll(clientImage, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	var img images.Image
+	var img edgecloudV2.Image
 	for _, i := range imgs {
-		if i.OsDistro == osDistroTest {
+		if i.OSDistro == osDistroTest {
 			img = i
 			break
 		}
@@ -50,67 +38,41 @@ func TestAccInstanceDataSource(t *testing.T) {
 		t.Fatalf("images with os_distro='%s' does not exist", osDistroTest)
 	}
 
-	optsV := volumes.CreateOpts{
+	optsV := edgecloudV2.VolumeCreateRequest{
 		Name:     volumeTestName,
 		Size:     volumeSizeTest * 5,
-		Source:   volumes.Image,
-		TypeName: volumes.Standard,
+		Source:   edgecloudV2.VolumeSourceImage,
+		TypeName: edgecloudV2.VolumeTypeStandard,
 		ImageID:  img.ID,
 	}
-	volumeID, err := createTestVolume(clientVolume, optsV)
+
+	volumeID, err := createTestVolumeV2(ctx, cfg.CloudClient, &optsV)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	client, err := createTestClient(cfg.Provider, edgecenter.InstancePoint, edgecenter.VersionPointV2)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	clientV1, err := createTestClient(cfg.Provider, edgecenter.InstancePoint, edgecenter.VersionPointV1)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	opts := instances.CreateOpts{
+	opts := edgecloudV2.InstanceCreateRequest{
 		Names:  []string{instanceTestName},
 		Flavor: flavorTest,
-		Volumes: []instances.CreateVolumeOpts{{
-			Source:    types.ExistingVolume,
+		Volumes: []edgecloudV2.InstanceVolumeCreate{{
+			Source:    edgecloudV2.VolumeSourceExistingVolume,
 			BootIndex: 0,
 			VolumeID:  volumeID,
 		}},
-		Interfaces: []instances.InterfaceInstanceCreateOpts{
+		Interfaces: []edgecloudV2.InstanceInterface{
 			{
-				InterfaceOpts:  instances.InterfaceOpts{Type: types.ExternalInterfaceType},
-				SecurityGroups: []edgecloud.ItemID{},
+				Type:           edgecloudV2.InterfaceTypeExternal,
+				SecurityGroups: []edgecloudV2.ID{},
 			},
 		},
 	}
 
-	res, err := instances.Create(client, opts).Extract()
+	taskResultCreate, err := utilV2.ExecuteAndExtractTaskResult(ctx, cfg.CloudClient.Instances.Create, &opts, cfg.CloudClient)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	taskID := res.Tasks[0]
-	instanceID, err := tasks.WaitTaskAndReturnResult(clientVolume, taskID, true, edgecenter.InstanceCreatingTimeout, func(task tasks.TaskID) (interface{}, error) {
-		taskInfo, err := tasks.Get(clientVolume, string(task)).Extract()
-		if err != nil {
-			return nil, fmt.Errorf("cannot get task with ID: %s. Error: %w", task, err)
-		}
-		id, err := instances.ExtractInstanceIDFromTask(taskInfo)
-		if err != nil {
-			return nil, fmt.Errorf("cannot retrieve instance ID from task info: %w", err)
-		}
-		return id, nil
-	},
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	defer instances.Delete(clientV1, instanceID.(string), instances.DeleteOpts{Volumes: []string{volumeID}})
+	instanceID := taskResultCreate.Instances[0]
 
 	resourceName := "data.edgecenter_instance.acctest"
 	tpl := func(name string) string {
@@ -132,9 +94,26 @@ func TestAccInstanceDataSource(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckResourceExists(resourceName),
 					resource.TestCheckResourceAttr(resourceName, "name", instanceTestName),
-					resource.TestCheckResourceAttr(resourceName, "id", instanceID.(string)),
+					resource.TestCheckResourceAttr(resourceName, "id", instanceID),
 				),
 			},
 		},
 	})
+	optsInstDel := edgecloudV2.InstanceDeleteOptions{
+		Volumes: []string{volumeID},
+	}
+
+	taskResultDelete, _, err := cfg.CloudClient.Instances.Delete(ctx, instanceID, &optsInstDel)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = utilV2.WaitAndGetTaskInfo(ctx, cfg.CloudClient, taskResultDelete.Tasks[0])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := utilV2.ResourceIsDeleted(ctx, cfg.CloudClient.Instances.Get, instanceID); err != nil {
+		t.Fatal(err)
+	}
+
 }
