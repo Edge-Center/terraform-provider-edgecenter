8b6d90ce584fd2487040dbb1f465a13880de649e
damir.zinatullin <damir.zinatullin@edgecenter.ru>
2024-02-07 14:42:30 +0300

    refactor(CLOUDDEV-537): changed go client in router resource

--- edgecenter/utils_router.go
+++ edgecenter/utils_router.go
@@ -13,6 +13,7 @@ import (
 	edgecloud "github.com/Edge-Center/edgecentercloud-go"
 	"github.com/Edge-Center/edgecentercloud-go/edgecenter/router/v1/routers"
 	"github.com/Edge-Center/edgecentercloud-go/edgecenter/subnet/v1/subnets"
+	edgecloudV2 "github.com/Edge-Center/edgecentercloud-go/v2"
 )
 
 var routerDecoderConfig = &mapstructure.DecoderConfig{
@@ -32,6 +33,39 @@ func StringToNetHookFunc() mapstructure.DecodeHookFuncType {
 		switch t {
 		case reflect.TypeOf(edgecloud.CIDR{}):
 			var ecCIDR edgecloud.CIDR
+			_, ipNet, err := net.ParseCIDR(data.(string))
+			if err != nil {
+				return nil, err
+			}
+			ecCIDR.IP = ipNet.IP
+			ecCIDR.Mask = ipNet.Mask
+			return ecCIDR, nil
+		case reflect.TypeOf(net.IP{}):
+			ip := net.ParseIP(data.(string))
+			if ip == nil {
+				return nil, fmt.Errorf("failed parsing ip %v", data)
+			}
+			return ip, nil
+		default:
+			// If the target type is not supported, return the data as is.
+			return data, nil
+		}
+	}
+}
+
+// StringToNetHookFuncV2 returns a DecodeHookFunc for the mapstructure package to handle the custom
+// conversion of string values to net.IP and edgecloudV2.CIDR types.
+func StringToNetHookFuncV2() mapstructure.DecodeHookFuncType {
+	return func(f reflect.Type, t reflect.Type, data interface{}) (interface{}, error) {
+		// Only process strings as source type.
+		if f.Kind() != reflect.String {
+			return data, nil
+		}
+
+		// Process the target types.
+		switch t {
+		case reflect.TypeOf(edgecloudV2.CIDR{}):
+			var ecCIDR edgecloudV2.CIDR
 			_, ipNet, err := net.ParseCIDR(data.(string))
 			if err != nil {
 				return nil, err
@@ -75,6 +109,29 @@ func extractHostRoutesMap(v []interface{}) ([]subnets.HostRoute, error) {
 	return hostRoutes, nil
 }
 
+// extractHostRoutesMapV2 converts a slice of interface{} representing host routes into a slice of edgecloudV2.HostRoute.
+func extractHostRoutesMapV2(v []interface{}) ([]edgecloudV2.HostRoute, error) {
+	decoderConfig := &mapstructure.DecoderConfig{
+		DecodeHook: StringToNetHookFuncV2(),
+	}
+
+	hostRoutes := make([]edgecloudV2.HostRoute, len(v))
+	for i, hostRoute := range v {
+		hs, ok := hostRoute.(map[string]interface{})
+		if !ok {
+			return nil, fmt.Errorf("failed to assert host route as map[string]interface{}")
+		}
+		var H edgecloudV2.HostRoute
+		err := MapStructureDecoder(&H, &hs, decoderConfig)
+		if err != nil {
+			return nil, err
+		}
+		hostRoutes[i] = H
+	}
+
+	return hostRoutes, nil
+}
+
 // routerInterfaceUniqueID generates a unique ID for a router interface using its subnet ID.
 func routerInterfaceUniqueID(i interface{}) int {
 	e := i.(map[string]interface{})
@@ -104,17 +161,34 @@ func extractExternalGatewayInfoMap(gw []interface{}) (routers.GatewayInfo, error
 	return gwInfo, nil
 }
 
-// extractInterfacesMap converts a slice of interface{} representing router interfaces
-// into a slice of routers.Interface using the provided mapstructure decoder configuration.
-func extractInterfacesMap(interfaces []interface{}) ([]routers.Interface, error) {
-	ifaceList := make([]routers.Interface, len(interfaces))
+// extractExternalGatewayInfoMapV2 converts the first element of a gateway slice
+// into a edgecloudV2.ExternalGatewayInfoCreate struct using the provided mapstructure decoder configuration.
+func extractExternalGatewayInfoMapV2(gw []interface{}) (edgecloudV2.ExternalGatewayInfoCreate, error) {
+	gateway, ok := gw[0].(map[string]interface{})
+	if !ok {
+		return edgecloudV2.ExternalGatewayInfoCreate{}, fmt.Errorf("failed to assert gateway as map[string]interface{}")
+	}
+
+	var gwInfo edgecloudV2.ExternalGatewayInfoCreate
+	err := MapStructureDecoder(&gwInfo, &gateway, routerDecoderConfig)
+	if err != nil {
+		return edgecloudV2.ExternalGatewayInfoCreate{}, err
+	}
+
+	return gwInfo, nil
+}
+
+// extractInterfacesMapV2 converts a slice of interface{} representing router interfaces
+// into a slice of edgecloudV2.RouterInterfaceCreate using the provided mapstructure decoder configuration.
+func extractInterfacesMapV2(interfaces []interface{}) ([]edgecloudV2.RouterInterfaceCreate, error) {
+	ifaceList := make([]edgecloudV2.RouterInterfaceCreate, len(interfaces))
 	for i, iface := range interfaces {
 		inter, ok := iface.(map[string]interface{})
 		if !ok {
 			return nil, fmt.Errorf("failed to assert interface as map[string]interface{}")
 		}
 
-		var ifaceInfo routers.Interface
+		var ifaceInfo edgecloudV2.RouterInterfaceCreate
 		err := MapStructureDecoder(&ifaceInfo, &inter, routerDecoderConfig)
 		if err != nil {
 			return nil, err
