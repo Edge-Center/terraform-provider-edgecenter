340e9625070559745df53beee0bddd6f9cfba276
Sergey Davydov <sergey.davydov@edgecenter.ru>
2024-03-04 16:41:53 +0400

    feat(CLOUDDEV-568): added file for test data source snapshot

--- edgecenter/test/data_source_edgecenter_snapshot_test.go
+++ edgecenter/test/data_source_edgecenter_snapshot_test.go
@@ -3,9 +3,14 @@
 package edgecenter_test
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
-	"github.com/Edge-Center/terraform-provider-edgecenter/edgecenter"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	edgecloudV2 "github.com/Edge-Center/edgecentercloud-go/v2"
+	utilV2 "github.com/Edge-Center/edgecentercloud-go/v2/util"
 )
 
 func TestAccSnapshotDataSource(t *testing.T) {
@@ -14,36 +19,31 @@ func TestAccSnapshotDataSource(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	client, err := createTestClient(cfg.Provider, edgecenter.VolumesPoint, edgecenter.VersionPointV1)
-	if err != nil {
-		t.Fatal(err)
-	}
+	ctx := context.Background()
 
-	opts := edgecenter.CreateVolumeOpts{
+	volumeOpts := edgecloudV2.VolumeCreateRequest{
 		Name:     volumeTestName,
 		Size:     volumeSizeTest,
-		Source:   edgecenter.NewVolume,
-		TypeName: edgecenter.Standard,
+		Source:   edgecloudV2.VolumeSourceNewVolume,
+		TypeName: edgecloudV2.VolumeTypeStandard,
 	}
 
-	volumeID, err := createTestVolume(client, opts)
+	volumeID, err := createTestVolumeV2(ctx, cfg.CloudClient, &volumeOpts)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	defer edgecenter.DeleteVolume(client, volumeID, edgecenter.DeleteVolumeOpts{})
-
-	snapshotOpts := edgecenter.CreateSnapshotOpts{
-		Name:     "snapshot_" + volumeTestName,
+	snapshotOpts := edgecloudV2.SnapshotCreateRequest{
+		Name:     "snapshot-" + volumeTestName,
 		VolumeID: volumeID,
 	}
 
-	snapshotID, err := createTestSnapshot(client, snapshotOpts)
+	taskResultCreate, err := utilV2.ExecuteAndExtractTaskResult(ctx, cfg.CloudClient.Snapshots.Create, &snapshotOpts, cfg.CloudClient)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	defer edgecenter.DeleteSnapshot(client, snapshotID, edgecenter.DeleteSnapshotOpts{})
+	snapshotID := taskResultCreate.Snapshots[0]
 
 	resourceName := "data.edgecenter_snapshot.acctest"
 	tpl := func(name string) string {
@@ -65,9 +65,35 @@ func TestAccSnapshotDataSource(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckResourceExists(resourceName),
 					resource.TestCheckResourceAttr(resourceName, "name", snapshotOpts.Name),
-					resource.TestCheckResourceAttr(resourceName, "volume_id", volumeID),
+					resource.TestCheckResourceAttr(resourceName, "snapshot_id", snapshotID),
 				),
 			},
 		},
 	})
+
+	taskSnapshotsDelete, _, err := cfg.CloudClient.Snapshots.Delete(ctx, snapshotID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = utilV2.WaitAndGetTaskInfo(ctx, cfg.CloudClient, taskSnapshotsDelete.Tasks[0])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := utilV2.ResourceIsDeleted(ctx, cfg.CloudClient.Snapshots.Get, snapshotID); err != nil {
+		t.Fatal(err)
+	}
+
+	taskVolumesDelete, _, err := cfg.CloudClient.Volumes.Delete(ctx, volumeID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = utilV2.WaitAndGetTaskInfo(ctx, cfg.CloudClient, taskVolumesDelete.Tasks[0])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := utilV2.ResourceIsDeleted(ctx, cfg.CloudClient.Volumes.Get, volumeID); err != nil {
+		t.Fatal(err)
+	}
 }
