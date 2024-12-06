36d32ff235da3f393c47ab8e9268aa5346a46b22
Aleksandr Semenov <aleksandr.semenov@edgecenter.ru>
2024-12-06 14:22:30 +0300

    chore(CLOUDDEV-1074): Remove and fix examples

--- examples/resources/edgecenter_cdn_resource/resource.tf
+++ examples/resources/edgecenter_cdn_resource/resource.tf
@@ -2,6 +2,20 @@ provider "edgecenter" {
   permanent_api_token = "251$d3361.............1b35f26d8"
 }
 
+resource "edgecenter_cdn_origingroup" "origin_group_1" {
+  name     = "origin_group_1"
+  use_next = true
+  origin {
+    source  = "example.com"
+    enabled = true
+  }
+  origin {
+    source  = "mirror.example.com"
+    enabled = true
+    backup  = true
+  }
+  consistent_balancing = true
+}
 
 resource "edgecenter_cdn_resource" "cdn_example_com" {
   cname               = "cdn.example.com"
