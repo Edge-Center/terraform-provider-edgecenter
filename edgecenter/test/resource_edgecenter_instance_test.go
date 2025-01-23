7116ca547da504b353a137b26dbc99df9ed4f4d5
Aleksandr Semenov <aleksandr.semenov@edgecenter.ru>
2025-01-23 17:15:05 +0300

    fix(CLOUDDEV-1286): Fix tests

--- edgecenter/test/resource_edgecenter_instance_test.go
+++ edgecenter/test/resource_edgecenter_instance_test.go
@@ -19,6 +19,10 @@ import (
 )
 
 func TestAccInstance(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+
 	cfg, err := createTestConfig()
 	if err != nil {
 		t.Fatal(err)
