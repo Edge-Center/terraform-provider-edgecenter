317f38b3d5c86a992d459b0cae50177b0e4c5d95
Aleksandr Semenov <aleksandr.semenov@edgecenter.ru>
2025-03-20 11:34:14 +0300

    fix(CLOUDDEV-1134): Create security group without port range

--- edgecenter/utils_securitygroup.go
+++ edgecenter/utils_securitygroup.go
@@ -3,11 +3,26 @@ package edgecenter
 import (
 	"crypto/md5"
 	"encoding/binary"
+	"errors"
+	"fmt"
 	"io"
 	"strconv"
 
 	edgecloudV2 "github.com/Edge-Center/edgecentercloud-go/v2"
 )
+
+type (
+	portRangeMax = int
+	portRangeMin = int
+)
+
+var networkProtocolWithPort = map[edgecloudV2.SecurityGroupRuleProtocol]struct{}{
+	edgecloudV2.SGRuleProtocolTCP:     {},
+	edgecloudV2.SGRuleProtocolUDP:     {},
+	edgecloudV2.SGRuleProtocolUDPLITE: {},
+	edgecloudV2.SGRuleProtocolSCTP:    {},
+	edgecloudV2.SGRuleProtocolDCCP:    {},
+}
 
 // secGroupUniqueID generates a unique ID for a security group rule using its properties.
 func secGroupUniqueID(i interface{}) int {
@@ -27,14 +42,22 @@ func secGroupUniqueID(i interface{}) int {
 }
 
 // extractSecurityGroupRuleCreateRequestV2 creates a security group rule from the provided map and security group ID.
-func extractSecurityGroupRuleCreateRequestV2(r interface{}, gid string) edgecloudV2.RuleCreateRequest {
+func extractSecurityGroupRuleCreateRequestV2(r interface{}, gid string) (edgecloudV2.RuleCreateRequest, error) {
+	var err error
 	rule := r.(map[string]interface{})
+
+	protocol := edgecloudV2.SecurityGroupRuleProtocol(rule["protocol"].(string))
 
 	opts := edgecloudV2.RuleCreateRequest{
 		Direction:       edgecloudV2.SecurityGroupRuleDirection(rule["direction"].(string)),
 		EtherType:       edgecloudV2.EtherType(rule["ethertype"].(string)),
-		Protocol:        edgecloudV2.SecurityGroupRuleProtocol(rule["protocol"].(string)),
+		Protocol:        protocol,
 		SecurityGroupID: &gid,
+	}
+
+	opts.PortRangeMin, opts.PortRangeMax, err = validatePortRange(protocol, rule)
+	if err != nil {
+		return edgecloudV2.RuleCreateRequest{}, err
 	}
 
 	minP, maxP := rule["port_range_min"].(int), rule["port_range_max"].(int)
@@ -51,25 +74,28 @@ func extractSecurityGroupRuleCreateRequestV2(r interface{}, gid string) edgeclou
 		opts.RemoteIPPrefix = &remoteIPPrefix
 	}
 
-	return opts
+	return opts, nil
 }
 
 // extractSecurityGroupRuleUpdateRequestV2 creates a security group rule from the provided map and security group ID.
-func extractSecurityGroupRuleUpdateRequestV2(r interface{}, gid string) edgecloudV2.RuleUpdateRequest {
+func extractSecurityGroupRuleUpdateRequestV2(r interface{}, gid string) (edgecloudV2.RuleUpdateRequest, error) {
 	rule := r.(map[string]interface{})
+
+	protocol := edgecloudV2.SecurityGroupRuleProtocol(rule["protocol"].(string))
 
 	opts := edgecloudV2.RuleUpdateRequest{
 		Direction:       edgecloudV2.SecurityGroupRuleDirection(rule["direction"].(string)),
 		EtherType:       edgecloudV2.EtherType(rule["ethertype"].(string)),
-		Protocol:        edgecloudV2.SecurityGroupRuleProtocol(rule["protocol"].(string)),
+		Protocol:        protocol,
 		SecurityGroupID: gid,
 	}
 
-	minP, maxP := rule["port_range_min"].(int), rule["port_range_max"].(int)
-	if minP != 0 && maxP != 0 {
-		opts.PortRangeMin = minP
-		opts.PortRangeMax = maxP
+	portRangeMin, portRangeMax, err := validatePortRange(protocol, rule)
+	if err != nil {
+		return edgecloudV2.RuleUpdateRequest{}, err
 	}
+
+	opts.PortRangeMin, opts.PortRangeMax = *portRangeMin, *portRangeMax
 
 	description, _ := rule["description"].(string)
 	opts.Description = description
@@ -79,5 +105,34 @@ func extractSecurityGroupRuleUpdateRequestV2(r interface{}, gid string) edgeclou
 		opts.RemoteIPPrefix = remoteIPPrefix
 	}
 
-	return opts
+	return opts, nil
+}
+
+// validatePortRange checks the validity of the port range specified in a security group rule for a given network protocol.
+//
+// Returns:
+// - A pointer portRangeMin (which is an int alias) for the minimum port value, or nil if not applicable.
+// - A pointer portRangeMax (which is an int alias) for the maximum port value, or nil if not applicable.
+// - An error if any validation fails, or nil if all validations pass.
+func validatePortRange(protocol edgecloudV2.SecurityGroupRuleProtocol, rule map[string]interface{}) (*portRangeMin, *portRangeMax, error) {
+	portRangeMin := rule["port_range_min"].(portRangeMin)
+	portRangeMax := rule["port_range_max"].(portRangeMax)
+
+	if _, ok := networkProtocolWithPort[protocol]; ok {
+		if portRangeMin == 0 || portRangeMax == 0 {
+			return nil, nil, errors.New("port range min/max not specified")
+		}
+
+		if portRangeMin > portRangeMax {
+			return nil, nil, errors.New("value of the port_range_min cannot be greater than port_range_max")
+		}
+
+		return &portRangeMin, &portRangeMax, nil
+	}
+
+	if portRangeMin != 0 || portRangeMax != 0 {
+		return nil, nil, fmt.Errorf("%s network protocol does not support ports", protocol)
+	}
+
+	return nil, nil, nil
 }
