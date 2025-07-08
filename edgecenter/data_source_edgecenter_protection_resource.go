4d3842b871aee483eab3e2e5f47b166f3316e4cc
Platon Ryzhikov <ihummer63@yandex.ru>
2025-07-08 14:21:35 +0300

    cty no longer used

--- edgecenter/data_source_edgecenter_protection_resource.go
+++ edgecenter/data_source_edgecenter_protection_resource.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"strconv"
 
-	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
