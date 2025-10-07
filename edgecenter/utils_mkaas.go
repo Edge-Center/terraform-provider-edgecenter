5bb83f8a13bbdd819e036bd8dc725c94d0fe3e09
Segrey Davydov <37906158+Uiopas@users.noreply.github.com>
2025-10-07 21:07:16 +0400

    feat(CLOUDDEV-2246): new mkaas_cluster and mkaas_pool resources

---
+++ edgecenter/utils_mkaas.go
@@ -0,0 +1,15 @@
+package edgecenter
+
+import edgecloudV2 "github.com/Edge-Center/edgecentercloud-go/v2"
+
+func flattenTaints(taints []edgecloudV2.MkaaSTaint) []map[string]interface{} {
+	result := make([]map[string]interface{}, 0, len(taints))
+	for _, t := range taints {
+		result = append(result, map[string]interface{}{
+			"key":    t.Key,
+			"value":  t.Value,
+			"effect": t.Effect,
+		})
+	}
+	return result
+}
