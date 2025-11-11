b96f879d7f375467c34d6d4ab32aa1e87d39cb67
Leonid Stepanov <leonid.stepanov@edgecener.ru>
2025-11-11 17:41:31 +0300

    feat(CLOUDDEV-2246): based on my comments

--- examples/resources/edgecenter_mkaas_pool/resource.tf
+++ examples/resources/edgecenter_mkaas_pool/resource.tf
@@ -16,6 +16,6 @@ resource "edgecenter_mkaas_pool" "apps" {
   #   security_group_id = "b4a1b1d3-xxxx-xxxx-xxxx-1b2c3d4e5f6a"
 
 
-  project_id = 1399838
-  region_id  = "2403"
+  project_id = 1234
+  region_id  = "1234"
 }
