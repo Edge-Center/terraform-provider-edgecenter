f219c888fc366da52b7b91bef798e7470f4cbeeb
Leonid Stepanov <leonid.stepanov@edgecener.ru>
2025-11-13 11:48:52 +0300

    feat(CLOUDDEV-2246): based on comments

--- edgecenter/resource_edgecenter_mkaas_cluster.go
+++ edgecenter/resource_edgecenter_mkaas_cluster.go
@@ -249,7 +249,6 @@ func resourceMKaaSClusterRead(ctx context.Context, d *schema.ResourceData, m int
 	if err != nil {
 		if resp.StatusCode == http.StatusNotFound {
 			tflog.Warn(ctx, fmt.Sprintf("[WARN] Removing Mkaas cluster %s because resource doesn't exist anymore", d.Id()))
-			d.SetId("")
 			return nil
 		}
 
