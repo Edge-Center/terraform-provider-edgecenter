d6380a471ab001c6d6cef103e11e8d7d6c7c7837
damir.zinatullin <damir.zinatullin@edgecenter.ru>
2025-11-13 18:07:35 +0300

    feat(CLOUDDEV-2379): added publish_kube_api_to_internet attribute to mkaas_cluster resource

--- examples/resources/edgecenter_mkaas_cluster/resource.tf
+++ examples/resources/edgecenter_mkaas_cluster/resource.tf
@@ -3,12 +3,13 @@ provider "edgecenter" {
 }
 
 resource "edgecenter_mkaas_cluster" "example" {
-  name             = "my-cluster01"
-  project_id       = 1
-  region_id        = 1
-  ssh_keypair_name = "tf-keypair"
-  network_id       = "6bf878c1-1ce4-47c3-a39b-6b5f1d79bf25"
-  subnet_id        = "dc3a3ea9-86ae-47ad-a8e8-79df0ce04839"
+  name                         = "my-cluster01"
+  project_id                   = 1
+  region_id                    = 1
+  ssh_keypair_name             = "tf-keypair"
+  network_id                   = "6bf878c1-1ce4-47c3-a39b-6b5f1d79bf25"
+  subnet_id                    = "dc3a3ea9-86ae-47ad-a8e8-79df0ce04839"
+  publish_kube_api_to_internet = true
 
   control_plane {
     flavor      = "g3-standard-2-4"
