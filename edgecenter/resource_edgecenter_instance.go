64b4a75492d484aad9b08d07de086e74bc30914f
sergey.nesterenko <sergey.nesterenko@edgecenter.ru>
2025-12-09 10:26:40 +0300

    chore(CLOUDDEV-2535): Extend instance migration timeout to 60 minutes

--- edgecenter/resource_edgecenter_instance.go
+++ edgecenter/resource_edgecenter_instance.go
@@ -25,7 +25,7 @@ const (
 	InstanceCreateTimeout  = 10 * time.Minute
 	InstanceDeleteTimeout  = 10 * time.Minute
 	InstanceUpdateTimeout  = 10 * time.Minute
-	InstanceMigrateTimeout = 10 * time.Minute
+	InstanceMigrateTimeout = 60 * time.Minute
 	InstancePoint          = "instances"
 
 	InstanceVMStateActive  = "active"
